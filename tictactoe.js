f38056819ee5854e085876a90533cd7164ea557d
Cheri <mightypillow@tutanota.com>
2021-01-26 03:04:58 -0500

    Restart button completely resets game

--- tictactoe.js
+++ tictactoe.js
@@ -16,17 +16,27 @@ const Player = (mark) => {
     return backwardCounter.reduce((sum, n) => sum += n, 0);
   };
 
+  const reset = () => {
+    for(let i = 0; i < 3; i++) {
+      rowCounter[i] = 0;
+      columnCounter[i] = 0;
+      forwardCounter[i] = 0;
+      backwardCounter[i] = 0;
+    }
+  }
+
   return {
     getMark,
     countRow,
     countColumn,
     countForward,
-    countBackward
+    countBackward,
+    reset
   };
 };
 
 const board = (() => {
-  const board = {
+  const container = {
     0: [],
     1: [],
     2: []
@@ -34,14 +44,21 @@ const board = (() => {
   let counter = 0;
 
   const setSpot = (row, column, mark) => {
-    board[row][column] = mark;
+    container[row][column] = mark;
     counter++;
   };
   const isFull = () => counter >= 9;
+  const reset = () => {
+    container[0] = [];
+    container[1] = [];
+    container[2] = [];
+    counter = 0;
+  };
 
   return {
     setSpot,
-    isFull
+    isFull,
+    reset
   };
 })();
 
@@ -66,11 +83,17 @@ const game = (() => {
 
   const getCurrentPlayer = () => players[0];
   const hasWin = () => winner;
+  const reset = () => {
+    players.forEach(player => player.reset());
+    winner = false;
+    if(players[0].getMark() === "O") players.reverse();
+  };
 
   return {
     makeTurn,
     getCurrentPlayer,
-    hasWin
+    hasWin,
+    reset
   };
 })();
 
@@ -82,9 +105,23 @@ const displayController = (() => {
   tieDiv.textContent = "It's a Tie!";
   tieDiv.classList.add("bold");
 
-  const disableGame = () => {
-    document.querySelector("main > section").classList.add("disabled");
-  }; 
+  const squares = document.querySelectorAll("section > div");
+  const boardDisplay = document.querySelector("main > section");
+  const startOverlay = document.querySelector("#startOverlay");
+  
+  const reset = () => {
+    squares.forEach(square => {
+      square.textContent = "";
+      square.className = "";
+    });
+    boardDisplay.classList.remove("disabled");
+    if(winDiv.parentElement) winDiv.parentElement.removeChild(winDiv);
+    if(tieDiv.parentElement) tieDiv.parentElement.removeChild(tieDiv);
+    document.querySelector("#player-X").classList.remove("bold");
+    document.querySelector("#player-O").classList.remove("bold");
+    board.reset();
+    game.reset();
+  };
 
   const makeTurn = (square) => {
     const row = Number(square.dataset.coords[0]);
@@ -102,12 +139,12 @@ const displayController = (() => {
 
     if(game.hasWin()) {
       lastPlayer.appendChild(winDiv);
-      disableGame();
+      boardDisplay.classList.add("disabled");
     }
     else if(board.isFull()) {
       lastPlayer.classList.remove("bold");
       lastPlayer.parentElement.parentElement.appendChild(tieDiv);
-      disableGame();
+      boardDisplay.classList.add("disabled");
     }
     else {
       lastPlayer.classList.remove("bold");
@@ -115,20 +152,25 @@ const displayController = (() => {
     }
   };
 
-  document.querySelectorAll("section > div")
-    .forEach(square => square.addEventListener("click", () => {
-      if(!square.textContent) {
-        const mark = makeTurn(square);
-        handleOutcome(mark);      
-      }
+  squares.forEach(square => square.addEventListener("click", () => {
+    if(!square.textContent) {
+      const mark = makeTurn(square);
+      handleOutcome(mark);      
+    }
   }));
 
   document.querySelector("#startOverlay > button").addEventListener("click", () => {
     document.querySelector("#player-X > :first-child").textContent = 
-      document.querySelector("#player1").value;
+        document.querySelector("#player1").value;
     document.querySelector("#player-O > :first-child").textContent = 
-      document.querySelector("#player2").value;
-    document.querySelector("#startOverlay").style.display = "none";
+        document.querySelector("#player2").value;
+    startOverlay.style.display = "none";
+    document.querySelector("#player-X").classList.add("bold");
+  });
+
+  document.querySelector("#restart").addEventListener("click", () => {
+    reset();
+    startOverlay.style.display = "flex";
   });
 
 })();
